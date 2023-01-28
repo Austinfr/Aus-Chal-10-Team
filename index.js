1cb35f0b2deed99fe5aeb67a0eb27f99181dc282
Austie <wustinah20@gmail.com>
2023-01-28 03:48:22 -0800

    The questions are prompted successfully, I am done with index for the most part. Added a lot of code to htmlCreator however still haven't tested it. TODO: test htmlCreator and then write tests and record the video for use

--- index.js
+++ index.js
@@ -1,19 +1,144 @@
 const inquirer = require('inquirer');
 const htmlCreator = require('./src/htmlCreator');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+let teamName = '';
+let teamData = [];
 
 //need to get some way to loop and store the data each time
 
-inquirer.prompt([
-    {
+function init() {
 
-    },
-    {
+    //then we move onto the questions about the team
+    const askTeamQuestions = () => {
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'teamMember',
+                message: 'What kind of team member would you like to add?',
+                choices: ["Manager", "Engineer", "Intern"]
+            },
+            //manager questions
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is this manager\'s name?',
+                when: (answers) => answers.teamMember === "Manager"
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is this manager\'s id?',
+                when: (answers) => answers.teamMember === "Manager"
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is this manager\'s email?',
+                when: (answers) => answers.teamMember === "Manager"
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'What is this manager\'s office number?',
+                when: (answers) => answers.teamMember === "Manager"
+            },
+            //engineer questions
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is this engineer\'s name?',
+                when: (answers) => answers.teamMember === "Engineer"
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is this engineer\'s id?',
+                when: (answers) => answers.teamMember === "Engineer"
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is this engineer\'s email?',
+                when: (answers) => answers.teamMember === "Engineer"
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is this engineer\'s Github name?',
+                when: (answers) => answers.teamMember === "Engineer"
+            },
+            //intern questions
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is this intern\'s name?',
+                when: (answers) => answers.teamMember === "Intern"
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is this intern\'s id?',
+                when: (answers) => answers.teamMember === "Intern"
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is this intern\'s email?',
+                when: (answers) => answers.teamMember === "Intern"
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What is this intern\'s school?',
+                when: (answers) => answers.teamMember === "Intern"
+            },
+            //final question
+            {
+                type: 'confirm',
+                name: 'addMore',
+                message: 'Woul you like to add another team member?(y/n)'
+            }
 
-    },
-    {
-        
+        ]).then(data => {
+            //stores the data using the helper classes
+            switch(data.teamMember){
+                case "Manager":
+                    let tempManager = new Manager(data.name, data.id, data.email, data.officeNumber);
+                    teamData.push(tempManager);
+                    break;
+                case "Engineer":
+                    let tempEngineer = new Engineer(data.name, data.id, data.email, data.github);
+                    teamData.push(tempEngineer);
+                    break;
+                case "Intern":
+                    let tempIntern = new Intern(data.name, data.id, data.email, data.school);
+                    teamData.push(tempIntern);
+                    break;
+            }
+
+            //if we want to add another team member we store our current data and recurse
+            if(data.addMore){
+                askTeamQuestions();
+
+            //for when we're done adding team members add the data and pass it to our html creator
+            }else{
+                htmlCreator.createPage(teamName, teamData);
+            }
+        })
     }
 
-]).then(data => {
-
-})
+    //asks about the team name first
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'teamName',
+            message: 'What is your team name?'
+        }
+    ]).then(data => {
+        teamName = data.teamName;
+        askTeamQuestions();
+    });
+}
+init();
--- src/htmlCreator.js
+++ src/htmlCreator.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const Engineer = require('../lib/Engineer');
+const Manager = require('../lib/Manager');
+const Intern = require('../lib/Intern');
 //makes the css just by requiring it
 // const css = require('./cssCreator');
 
@@ -18,24 +21,80 @@ function createHeader(name, title){
             throw new Error("You put something wrong");
     }
 
-    return [`<h2>${name}</h2>`,`<p>${title}</p>`];
+    return `<div class="bg-primary text-white">\n<h2>${name}</h2>\n<p>${title}</p>\n</div>`;
 }
 
-function createCard(data){
-    //header
-    let cardHeader = createHeader(data.name, data.title);
-    //list
-    let list = ``
-
+//the lists are always in 3 thankfully
+function createList(listItems){
+    return `<ul class="list-group bg-secondary">\n<li class="list-group-item">${listItems[0]}</li>\n<li class="list-group-item">${listItems[1]}</li>\n<li class="list-group-item">${listItems[2]}</li>`;
 }
 
-function createPage(data){
-    //create the page header
-
-    //create a card for each item given
-    for(let databit of data){
-        createCard(databit);
+//based on what type of employee it will format the text to match
+function speciality(employee){
+    if(employee instanceof Manager){
+        return `Office number: ${employee.getOfficeNumber()}`;
+    }
+    if(employee instanceof Engineer){
+        return `Github: <a href="https://github.com/${employee.getGithub()}" target="_blank">${employee.getGithub()}</a></li>`;
+    }
+    if(employee instanceof Intern){
+        return `School: ${employee.getSchool()}`;
     }
 }
 
-// module.exports = createPage;
+function createCard(employee){
+    //header
+    let cardHeader = createHeader(employee.getName(), employee.getRole());
+    //list contains id, email, officenumber
+    let list = createList([
+        `ID: ${employee.getId()}`,
+        `Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a></li>`,
+        speciality(employee)
+    ])
+
+    return `<div class="${employee.getRole().toLowerCase()}-card">\n${cardHeader}\n${list}\n</div>`;
+
+}
+
+function createPage(name, data){
+    //create the page header
+    let pageHeader = `<header><h1>${name}</h1></header>`;
+
+    let cardSection = `<div id=team-cards class="d-flex justify-content-center>\n`;
+
+    //create a card for each item given
+    for(let databit of data){
+
+        //based off of what class it is
+        cardSection += `${createCard(databit)}\n`;
+    }
+
+    cardSection += `</div>`;
+
+    let head = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>`;
+
+    let body = `<body>\n${pageHeader}\n${cardSection}\n</body>`
+
+    let final = `${head}\n${body}\n</html>`;
+
+    fs.writeFile('../dist/index.html', final, 'utf8' | 'w', (err) => {
+        if(err){
+            throw err;
+        }else{
+            console.log("File Written successfully");
+        }
+    })
+}
+
+module.exports = {
+    createPage
+}
