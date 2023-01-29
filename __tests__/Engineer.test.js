1509c25ff5cc105fedc0e5b3ef086ec4578e28d1
Austie <wustinah20@gmail.com>
2023-01-29 15:31:18 -0800

    finished up engineer.test.js, all test classes are done. TODO readme and video explanation

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -10,4 +10,17 @@ test('Engineer getRole returns Engineer not Employee', () => {
     expect(new Engineer(0,0,0,0).getRole()).toBe('Engineer');
 });
 
-test('');
+test('Fumoto github returns the username fucode', () => {
+    expect(fumoto.getGithub()).toBe('fucode');
+    expect(fumoto.getGithub()).not.toBe('github');
+});
+
+test('Different engineers have different properties', () => {
+    let kenzie = new Engineer('Kenzie', 8, 'kenzington@yahoomail.com', 'kkenz');
+
+    expect(fumoto).not.toEqual(kenzie);
+    expect(kenzie.getName()).not.toBe(fumoto.getName());
+    expect(fumoto.getId()).not.toBe(kenzie.getId());
+    expect(fumoto.getGithub()).not.toBe(kenzie.getGithub());
+    expect(fumoto.getRole()).toBe(kenzie.getRole());
+})
